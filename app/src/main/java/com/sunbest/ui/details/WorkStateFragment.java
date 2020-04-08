f429b35592b282ecbd1909bc52a8afe3985acca4
钱旭 <1464238196@qq.com>
2020-04-08 18:34:39 +0800

    更改部分ui

--- app/src/main/java/com/sunbest/ui/details/WorkStateFragment.java
+++ app/src/main/java/com/sunbest/ui/details/WorkStateFragment.java
@@ -2,6 +2,7 @@ package com.sunbest.ui.details;
 
 import android.os.Bundle;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,6 +10,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.sunbest.R;
+import com.sunbest.databinding.FragmentWorkStateBinding;
+
+import org.angmarch.views.NiceSpinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -24,7 +31,17 @@ public class WorkStateFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_work_state, container, false);
+        FragmentWorkStateBinding binding= DataBindingUtil.inflate(inflater,R.layout.fragment_work_state, container, false);
+        NiceSpinner niceSpinner1=binding.windowsSpinner1;
+        NiceSpinner niceSpinner2=binding.windowsSpinner2;
+
+        List<String> datasets=new ArrayList<>();
+        for (int i=0;i<4;i++){
+            datasets.add("天窗"+i);
+        }
+        niceSpinner1.attachDataSource(datasets);
+        niceSpinner2.attachDataSource(datasets);
+
+        return binding.getRoot();
     }
 }
